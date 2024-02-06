86c0ddcb010cab13a114e4c9a93d446b8cd1955e
khusanrashidov <rashidov.kb2030@gmail.com>
2024-02-07 01:25:31 +0500

    Arrays funciton.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,7 +6,26 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            ArrayClass.Arrays();
         }
     }
 }
 
+class ArrayClass
+{
+    // ehh
+    public static void Arrays()
+    {
+        long[] perStudentPetCount = [0, 1, 2, 0, 1, 2, 0, 2, 8];
+        long numOfStudents = perStudentPetCount.Length;
+        Console.WriteLine(numOfStudents);
+        long sum = 0;
+        foreach (long individualPetCount in perStudentPetCount)
+        {
+            sum += individualPetCount;
+        }
+        Console.WriteLine(sum);
+        long average = sum / numOfStudents;
+        Console.WriteLine(average); // we get integer and this is good because we cannot have half of a pet
+    }
+}
